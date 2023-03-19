231e01d40557f8e3ab21fc5f568ffe36a068268c
DmytroKryvoruchenko <dmytro.krivoruchenko@gmail.com>
2023-03-19 14:08:11 +0200

    Added my lab 2

--- lab2_docker_sample/sample_query_1.sql
+++ lab2_docker_sample/sample_query_1.sql
@@ -1,3 +1,8 @@
 -- sample query 1
-SELECT * FROM artists;
+SELECT * FROM "Chats" LIMIT 1;
+SELECT * FROM "Messages" LIMIT 1;
+SELECT * FROM "MessageDetails" LIMIT 1;
+SELECT * FROM "Media" LIMIT 1;
+SELECT * FROM "MessageKeyPhrases" LIMIT 1;
+SELECT * FROM "KeyPhrases" LIMIT 1;
 
--- lab2_docker_sample/sample_query_2.sql
+++ lab2_docker_sample/sample_query_2.sql
@@ -1,3 +1,8 @@
 -- sample query 2
-SELECT * FROM albums;
+SELECT COUNT(*) as Total_Chatas FROM "Chats";
+SELECT COUNT(*) as Total_Messages FROM "Messages";
+SELECT COUNT(*) as Total_MessageDetails FROM "MessageDetails";
+SELECT COUNT(*) as Total_Media FROM "Media";
+SELECT COUNT(*) as Total_MessageKeyPhrases FROM "MessageKeyPhrases";
+SELECT COUNT(*) as Total_KeyPhrases FROM "KeyPhrases";
 
